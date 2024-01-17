bb483e3ecc7ed78f47d3ab0fdafc6a05ee2744fa
Ann <59127075+kotogavr@users.noreply.github.com>
2024-01-17 12:58:01 +0800

    init

---
+++ astro.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  site: process.env.CI
+    ? "https://kotogavr.vercel.app"
+    : "http://localhost:4321",
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+  ],
+});
