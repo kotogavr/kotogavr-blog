f387db9104f7dd5df9431c7c7c0e1ff9ae52925f
Ann <59127075+kotogavr@users.noreply.github.com>
2024-02-06 10:38:06 +0000

    Image fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,19 +4,22 @@ import { z, reference, defineCollection } from "astro:content";
 const Tags = z.enum(["Ruby on Rails", "indie making", "Tag3"]);
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    tags: z.array(Tags),
-    image: z
-      .object({
-        src: z.string().optional(),
-        alt: z.string().optional(),
-      })
-      .optional(),
-    publishDate: z.string(),
-    relatedPosts: z.array(reference("blog")).optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(Tags),
+      image: z
+        .object({
+          src: image().refine((img) => img.width > 1080, {
+            message: "Image width must be at least 1080px",
+          }),
+          alt: z.string().optional(),
+        })
+        .optional(),
+      publishDate: z.string(),
+      relatedPosts: z.array(reference("blog")).optional(),
+    }),
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
