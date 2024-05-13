029c544b3de7b829fc8e3b91c1a795993b7b44f3
Ann <59127075+kotogavr@users.noreply.github.com>
2024-05-13 17:43:43 +0800

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -2,25 +2,20 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import vercel from "@astrojs/vercel/static";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? "https://kotogavr.vercel.app"
-    : "http://localhost:4321",
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    mdx(),
-  ],
+  site: process.env.CI ? "https://kotogavr.vercel.app" : "http://localhost:4321",
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), mdx(), sitemap()],
   output: "static",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
-  }),
+      enabled: true
+    }
+  })
 });
